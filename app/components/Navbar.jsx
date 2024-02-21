10db8c72bcb2b2ea9fb09e187060ae89289ff96f
sajeed11 <sajednemli39@gmail.com>
2024-02-21 23:29:49 +0100

    Update Navbar and layout styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../components/download.png';
 
 export default function Navbar() {
     return (
         <nav>
-            <h1>Red Helpdesk</h1>
+            <Image
+                src={logo}
+                alt="SJ11 Logo"
+                width={70}
+                quality={100}
+                placeholder='blur'
+            />
+            <h1>SJ11 Helpdesk</h1>
             <Link href="/">Dashboard</Link>
             <Link href="/tickets">Tickets</Link>
         </nav>
