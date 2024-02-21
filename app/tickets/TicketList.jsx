1add691973ef9c9caa9db268668e3ab47260d220
sajeed11 <sajednemli39@gmail.com>
2024-02-21 23:30:11 +0100

    Add ticket list and ticket details components

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+// Fetching function
+async function getTickets() {
+	const res = await fetch('http://localhost:4000/tickets', {
+		next: {
+			revalidate: 0
+		}
+	});
+
+	return res.json();
+}
+
+export default async function TicketList() {
+	const tickets = await getTickets();
+
+	return (
+		<>
+			{tickets.map((ticket) => (
+				<div
+					key={ticket.id}
+					className="card my-5"
+				>
+					<Link href={`/tickets/${ticket.id}`}>
+						<h3>
+							{ticket.title}
+						</h3>
+						<p>
+							{ticket.body.slice(0, 200)}...
+						</p>
+						<div className={`pill ${ticket.priority}`}>
+							{ticket.priority} Priority
+						</div>
+					</Link>
+				</div>
+			))}
+			{tickets.length === 0 && (
+				<p className="text-center">
+					No tickets to display, :(
+				</p>
+			)}
+		</>
+	)
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,7 +1,22 @@
-export default function Tickets(){
-    return (
-        <main>
-            <h1>Tickets</h1>
-        </main>
-    )
+import TicketList from "./TicketList";
+
+export default function Tickets() {
+	return (
+		<main>
+			<nav>
+				<div>
+					<h2>
+						Tickets
+					</h2>
+					<p>
+						<small>
+							Currently open tickets.
+						</small>
+					</p>
+				</div>
+			</nav>
+
+			<TicketList />
+		</main>
+	)
 }
