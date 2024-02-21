38494451993968ea6ed738aa24b58bac0bc739b9
sajeed11 <sajednemli39@gmail.com>
2024-02-21 19:37:07 +0100

    Add Navbar component and RootLayout to the project

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+// Components
+import Navbar from './components/Navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Learn Next',
+  description: 'Generated with create-next-app',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
